85473b42e49148062e78ef9e7fc14af7d28ca67f
geauxvirtual <justin.guidroz@gmail.com>
2021-11-10 20:12:27 -0800

    Add page support for common pages 50,51,52

--- src/device.rs
+++ src/device.rs
@@ -8,6 +8,8 @@ pub mod weightscale;
 use hrm::HeartRateMonitor;
 use weightscale::WeightScale;
 
+use crate::message::{bytes_to_u16, bytes_to_u32};
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Device {
     WeightScale(WeightScale),
@@ -70,5 +72,93 @@ impl Device {
             Device::HeartRateMonitor(device) => device.transmission_type(),
             Device::PowerMeter(device) => device.transmission_type(),
         }
+    }
+}
+
+// Common data pages across device types.
+// Page 0x50 - Manufacturer Information
+pub struct Page0x50([u8; 8]);
+
+impl Page0x50 {
+    pub fn hardware_revision(&self) -> u8 {
+        self.0[3]
+    }
+
+    pub fn manufacturer_id(&self) -> u16 {
+        bytes_to_u16(&self.0[4..6])
+    }
+
+    pub fn model_number(&self) -> u16 {
+        bytes_to_u16(&self.0[6..])
+    }
+}
+// Page 0x51 - Product Information
+pub struct Page0x51([u8; 8]);
+
+impl Page0x51 {
+    pub fn software_version(&self) -> f32 {
+        // Check to see if supplemental version is valid
+        if self.0[2] == 0xFF {
+            self.0[3] as f32 / 10_f32
+        } else {
+            (self.0[3] * 100 + self.0[2]) as f32 / 1000_f32
+        }
+    }
+
+    pub fn serial_number(&self) -> u32 {
+        bytes_to_u32(&self.0[4..])
+    }
+}
+// Page 0x52 - Battery Status
+pub struct Page0x52([u8; 8]);
+
+impl Page0x52 {
+    // Battery Voltage in volts. If Coarse battery voltage equals 0xF, then
+    // fractional battery voltage will equal 0xFF as being invalid.
+    pub fn battery_voltage(&self) -> Option<f32> {
+        // If coarse voltage equals 0x0F, just return None
+        if self.coarse_voltage() == 0x0F {
+            return None;
+        }
+
+        Some(self.coarse_voltage() as f32 + (self.0[6] as f32 / 256_f32))
+    }
+
+    // Operating time in hours
+    pub fn operating_time(&self) -> f32 {
+        (bytes_to_u32(&self.0[3..6]) * self.time_resolution() as u32) as f32 / 3600_f32
+    }
+
+    pub fn battery_status(&self) -> &str {
+        if self.0[7] & 0x10 == 0x10 {
+            return "New";
+        }
+        if self.0[7] & 0x20 == 0x20 {
+            return "Good";
+        }
+        if self.0[7] & 0x30 == 0x30 {
+            return "Ok";
+        }
+        if self.0[7] & 0x40 == 0x40 {
+            return "Low";
+        }
+        if self.0[7] & 0x50 == 0x50 {
+            return "Critical";
+        }
+        // If our bit matching doesn't match anything, just return "Invalid"
+        "Invalid"
+    }
+
+    // Time resolution for operating time in seconds.
+    fn time_resolution(&self) -> u8 {
+        if self.0[7] & 0x80 == 0x80 {
+            16
+        } else {
+            2
+        }
+    }
+
+    fn coarse_voltage(&self) -> u8 {
+        self.0[7] & 0x0F
     }
 }
