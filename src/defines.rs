1aec11319f4ac15367b166f2d4650c2f6d5d6033
geauxvirtual <justin.guidroz@gmail.com>
2022-01-08 09:50:19 -0800

    Remove defines.rs. Move values to message.rs for now.

--- src/defines.rs
+++
@@ -1,4 +0,0 @@
-// TODO Simplify some of the constants used for encoding/decoding ANT+ protocol.
-pub const ANT_STANDARD_DATA_PAYLOAD_SIZE: usize = 8;
-pub const ANT_EXT_MESG_DEVICE_ID_FIELD_SIZE: usize = 4;
-pub const ANT_EXT_STRING_SIZE: usize = 27;
